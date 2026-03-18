4e89d13b42c1984fd9fc633cfc28ed48fa6e0f04
Timothy <Timothy.L9915@outlook.com>
2026-03-17 20:08:10 -0700

    Fixed delete movie function and adjusted values in add movie function

--- movie_manager.py
+++ movie_manager.py
@@ -6,11 +6,11 @@ import re
 def add_movie(file_name, movies):
     """Add Movies and check with the criteria"""
     title = input("Title: ")
-    if re.search(r"^[A-Z][a-z0-9]{32}$", title) is None:
+    if re.search(r"^[A-Z][a-z0-9]{1,32}$", title) is None:
         raise ValueError("Title must be no longer than 32 Characters")
 
     for title in movies:
-        if movies["title"] == title:
+        if movies["title"].lower() == title.lower():
             raise ValueError("Movie already exists")
 
     genre = input("Genre: ")
@@ -29,20 +29,20 @@ def add_movie(file_name, movies):
 
     # Add Type Error Here
 
-    rating = input("Rating: ")
-    if re.search(r"^[0-5]{1}$", rating) is None:
-        raise ValueError("Rating is invalid")
+    rating = float(input("Rating: "))
+    if rating < 0 or rating > 5:
+        raise ValueError("Rating must be between 0 and 5")
 
     description = input("Description: ")
-    if re.search(r"^\b[A-Z][a-z]\b${0,128}$") is None:
-        raise ValueError("Description can only be 128 characters long")
+    if len(description) > 128:
+        raise ValueError("Description must be 128 characters or less")
 
     movie = {
         "title": title,
         "genre": genre,
         "length": length,
-        "year": int(year),
-        "rating": float(rating),
+        "year": year,
+        "rating": rating,
         "description": description
     }
 
@@ -53,10 +53,20 @@ def add_movie(file_name, movies):
     print("Movie added successfully")
 
 
-def delete_movie(file_name, movies):
+def delete_movie(movies):
     """Deletes Movie from JSON file"""
     movie_to_delete = input("Enter the name of the movie: ")
 
+    is_deleted = False
+    for movie in movies:
+        if movies["title"] == movie_to_delete:
+            movies.remove(movie)
+            is_deleted = True
+            break
+    if is_deleted:
+        print("Movie Deleted")
+    else:
+        print("Movie not Found")
 
 
 def view_summary(file_name):
