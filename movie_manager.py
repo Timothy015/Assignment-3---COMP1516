daef45e2d9214a491d78e28c40892d5176940655
Timothy <Timothy.L9915@outlook.com>
2026-03-17 19:49:50 -0700

    Added Regex to Add Movie Function

--- movie_manager.py
+++ movie_manager.py
@@ -4,13 +4,38 @@ import re
 
 
 def add_movie(file_name, movies):
-
+    """Add Movies and check with the criteria"""
     title = input("Title: ")
+    if re.search(r"^[A-Z][a-z0-9]{32}$", title) is None:
+        raise ValueError("Title must be no longer than 32 Characters")
+
+    for title in movies:
+        if movies["title"] == title:
+            raise ValueError("Movie already exists")
+
     genre = input("Genre: ")
+    if re.search(r"^[A-Z] ?[a-z]*$", genre) is None:
+        raise ValueError("Genre is invalid")
+
     length = input("Length (HH:MM): ")
+    if re.search(r"^[0-99]{2}:[0-59]{2}$", length) is None:
+        raise ValueError("Length must be HH:MM")
+
+    # Add Type Error Here
+
     year = input("Year: ")
+    if re.search(r"^\d{4}$", year) is None:
+        raise ValueError("Year is invalid")
+
+    # Add Type Error Here
+
     rating = input("Rating: ")
+    if re.search(r"^[0-5]{1}$", rating) is None:
+        raise ValueError("Rating is invalid")
+
     description = input("Description: ")
+    if re.search(r"^\b[A-Z][a-z]\b${0,128}$") is None:
+        raise ValueError("Description can only be 128 characters long")
 
     movie = {
         "title": title,
@@ -30,17 +55,8 @@ def add_movie(file_name, movies):
 
 def delete_movie(file_name, movies):
     """Deletes Movie from JSON file"""
+    movie_to_delete = input("Enter the name of the movie: ")
 
-    title = input("Enter movie title to delete: ")
-
-    for movie in movies:
-        if movie["title"].lower() == title.lower():
-            movies.remove(movie)
-            file_io.write_movies(file_name, movies)
-            print("Movie deleted successfully")
-            return
-
-    print("Movie not Found!")
 
 
 def view_summary(file_name):
@@ -64,4 +80,3 @@ def title_search():
 def genre_search():
     """Searches JSON file by Genre"""
     pass
-
