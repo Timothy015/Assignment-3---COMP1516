e602b742c0d2b0cae90d7342fe863f8ce9cf600c
Timothy <Timothy.L9915@outlook.com>
2026-03-21 11:11:18 -0700

    Added doctrings to functions

--- file_io.py
+++ file_io.py
@@ -3,18 +3,26 @@ import os
 
 
 def read_movies(file_name):
-    """Reads movies from JSON file and returns a list"""
+    """Reads movies data from JSON File
+    :param file_name: The name of the file to read from
+    :return: A list of movie dictionaries"""
+
     if not os.path.exists(file_name):
         print("The movie database does not exist")
         return []
 
-    with open(file_name, 'r') as fh:
-        return json.load(fh)
+    try:
+        with open(file_name, 'r') as fh:
+            return json.load(fh)
+
+    except FileNotFoundError:
+        return []
 
 
 def write_movies(file_name, movies):
-    """Collect Movie Data and Write a file"""
+    """Writes movie data to JSON file
+    :param file_name: The name of the file to write to
+    :param movies: The list of movie dictionaries"""
 
     with open(file_name, 'w') as fh:
         json.dump(movies, fh, indent=4)
-
